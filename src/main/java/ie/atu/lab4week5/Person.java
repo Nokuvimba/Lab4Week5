0b01e962e744b85be53c3651ff25c3c583370486
Nokuvimba <G00423951@atu.ie>
2024-10-15 11:19:27 +0100

    used the Post METHOD while checking if they're any errors using the GlobalErrorHandler class.

--- src/main/java/ie/atu/lab4week5/Person.java
+++ src/main/java/ie/atu/lab4week5/Person.java
@@ -1,9 +1,27 @@
 package ie.atu.lab4week5;
 
-public class Person {
-    private String name;
-    private String title;
-    private int employeeId;
-    private int age;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Person {
+    @NotBlank(message = "name is not black")
+    private String name;
+    @NotBlank(message = "title is not black")
+    private String title;
+    @Min(value=1,message = "employeeId is not null")
+    private int employeeId;
+    @Min(value = 16,message = "age is not less that 16")
+    private int age;
+    @Email(message = "Invalid email address")
+    private String email;
+    @NotBlank(message = "position not blank")
+    private String position;
+    @NotBlank(message = "department not blank")
+    private  String department;
 }
